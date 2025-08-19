59b35d0c5d27eb805dbefa6ed3fd342571285b41
AlekseyPV <leha041304@gmail.com>
2025-08-19 19:14:45 +0300

    fix keyboard

--- keyboards/promotions_keyboard.py
+++ keyboards/promotions_keyboard.py
@@ -4,4 +4,4 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 def promotions_back_keyboard() -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardBuilder()
     kb.button(text="Назад")
-    return kb.as_markup()
+    return kb.as_markup(resize_keyboard=True)
